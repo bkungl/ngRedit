80e304ff9a0f042ec4f23e06d3213f87d181447e
bkungl <bkungl15@gmail.com>
2020-06-05 12:16:55 -0400

    Add files via upload

---
+++ subreddit-side/subreddit-side.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subreddit-side',
+  templateUrl: './subreddit-side.component.html',
+  styleUrls: ['./subreddit-side.component.css']
+})
+export class SubredditSideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
